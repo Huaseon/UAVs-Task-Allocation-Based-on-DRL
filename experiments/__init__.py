a63c1102a259472333cf51db9d249cb854b94298
dsj <dsj34473@163.com>
2025-07-16 10:08:15 +0800

    init

--- experiments/__init__.py
+++
@@ -1,6 +0,0 @@
-# %%
-from pathlib import Path
-
-# %%
-cur_path = Path(".") / "experiments"
-SEED = 20040508
--- experiments/main/env/data/__init__.py
+++
@@ -1,5 +0,0 @@
-# %%
-from .. import cur_path, SEED
-
-# %%
-cur_path = cur_path / "data"
